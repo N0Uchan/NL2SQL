034edd663ce08d67fa63bed4fdfdd602386f7ec3
Ronish <ronish.rohan@gmail.com>
2025-02-22 12:30:26 +0530

    Header

--- app/page.js
+++ app/page.js
@@ -1,16 +1,15 @@
 "use client";
 
-
 import { Button } from "@/components/ui/button";
 import React, { useRef, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { Send } from "lucide-react";
+import { RefreshCcw, Send } from "lucide-react";
 import Chat from "@/components/main/Chat";
 import Output from "@/components/main/Output";
 
 const page = () => {
   const fileInputRef = useRef(null);
-  const [isFileUploaded, setIsFileUploaded] = useState(false);
+  const [isFileUploaded, setIsFileUploaded] = useState(true);
 
   const handleButtonClick = () => {
     fileInputRef.current.click();
@@ -54,22 +53,30 @@ const page = () => {
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            className="size-full flex items-center justify-center"
-          >
-            <div className="flex items-center justify-center flex-col w-[50vw] h-full border-r-2 border-border relative" >
-              <Chat></Chat>
-            </div>
-            <div className="flex items-center justify-center flex-col w-[50vw] h-full" >
-              <Output></Output>
+            className="size-full flex flex-col items-center justify-center"
+          > 
+
+              <div className="flex h-16 shrink-0 w-full border-b-2 border-border items-center justify-center p-4" >
+
+                NL2SQL
+                <div className="ml-auto flex justify-center items-center gap-2" >
+                  <Button variant="outline">Upload another  </Button>
+                  <Button variant=""><RefreshCcw></RefreshCcw></Button>
+                </div>
+              </div>
+            <div className="flex h-full">
+              <div className="flex items-center justify-center flex-col w-[50vw] h-full border-r-2 border-border relative">
+                <Chat></Chat>
+              </div>
+              <div className="flex items-center justify-center flex-col w-[50vw] h-full">
+                <Output></Output>
+              </div>
             </div>
           </motion.div>
         )}
       </AnimatePresence>
-
     </div>
   );
 };
-
-
 
 export default page;
